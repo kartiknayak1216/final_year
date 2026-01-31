9cca8c6103dd6c125b41cf3ca017303f2c9ada67
kartiknayak1216 <nayakkartik1216@gmail.com>
2026-01-31 17:52:42 +0530

    01

---
+++ src/components/provider.tsx
@@ -0,0 +1,15 @@
+"use client"
+import { ThemeProvider } from "./theme-provider";
+
+export const Providers = ({ children }: { children: React.ReactNode }) => {
+return(
+ <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+    {children}
+</ThemeProvider>
+)
+}
